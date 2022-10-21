3f97e41d16347322c89d71120e3c6ef40eddb975
WolfgangA1 <washcraft10@gmail.com>
2022-10-21 16:02:31 -0400

    Add virtual pet application

---
+++ src/main/java/VirtualPet.java
@@ -0,0 +1,106 @@
+import virtual_pet.VirtualPetApplication;
+
+import java.util.Scanner;
+
+public class VirtualPet {
+    public static void main(String[] args) {
+        //Interact with a VirtualPet object in this method
+        int select;
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your cats name?");
+        String catName = input.nextLine();
+
+        VirtualPetApplication cat = new VirtualPetApplication(catName);
+
+        needsMenu(catName);
+
+        do {
+            System.out.println("What would you like to do today?");
+            System.out.println("Press 1 to give " + catName + " pets");
+            System.out.println("Press 2 to feed " + catName + " favorite treats");
+            System.out.println("Press 3 to nap " + catName + " cuddle");
+            System.out.println("Press 4 to give water to " + catName);
+            System.out.println("Press 5 to let " + catName + " use litter box");
+            System.out.println("Press 6 to quit.");
+
+            select = input.nextInt();
+
+            if (select == 6) {
+                continue;
+            } else if (select == 1) {
+                if (cat.getAttention() > 5) {
+                    System.out.println(catName + " doesn't want attention right now");
+                    continue;
+                }
+                System.out.println(catName + " starts to purrrr!");
+                cat.changeAttention(+1);
+                cat.changeHunger(-1);
+                cat.changeThirst(-1);
+                cat.changeSleepy(-1);
+                showCatCurrentNeeds(cat);
+            } else if (select == 2) {
+                if (cat.getHunger() > 5) {
+                    System.out.println(catName + " walks away from the treats");
+                    continue;
+                }
+                System.out.println(catName + " they plop down from being fed");
+                cat.changeThirst(-1);
+                cat.changeSleepy(-1);
+                cat.changeBathroom(-1);
+                cat.changeAttention(+1);
+                showCatCurrentNeeds(cat);
+            } else if (select == 3) {
+                if(cat.getSleepy() > 5){
+                    System.out.println(catName + " gets angry");
+                    continue;
+                }
+                System.out.println(catName + " meows and curls in a ball");
+                cat.changeSleepy(+1);
+                cat.changeBathroom(-1);
+                cat.changeHunger(-1);
+                cat.changeThirst(-1);
+                showCatCurrentNeeds(cat);
+            }else if (select == 4){
+                if (cat.getThirst() > 5){
+                    System.out.println(" Walks away from water bowl, not interested");
+                    continue;
+                }
+                System.out.println(catName + " enjoys the water");
+                cat.changeThirst(+1);
+                cat.changeBathroom(-1);
+                cat.changeHunger(0);
+                showCatCurrentNeeds(cat);
+            }else if (select == 5) {
+                if (cat.getBathroom() > 5){
+                    System.out.println(catName + "");
+                    continue;
+                }
+                System.out.println(catName + " had to go really bad");
+                cat.changeBathroom(+1);
+                cat.changeHunger(-1);
+                cat.changeSleepy(0);
+                showCatCurrentNeeds(cat);
+            } else {
+                System.out.println("You need to choose something to do with " + catName);
+            }
+            cat.tick();
+
+
+        }while (select != 6);
+
+    }
+    private static void showCatCurrentNeeds(VirtualPetApplication cat){
+        System.out.println("Attention: " + cat.getAttention());
+        System.out.println("Thirst: " + cat.getThirst());
+        System.out.println("Bathroom: " + cat.getBathroom());
+        System.out.println("Hunger:" + cat.getHunger());
+        System.out.println("Sleepy: " + cat.getSleepy());
+    }
+
+    public static void needsMenu(String catName) {
+
+    }
+
+}
+
+
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,9 +1,89 @@
 package virtual_pet;
 
 public class VirtualPetApplication {
+    private String catName;
+    private int hunger;
+    private int thirst;
+    private int attention;
+    private int bathroom;
+    private int sleepy;
 
-    public static void main(String[] args) {
-        //Interact with a VirtualPet object in this method
+
+
+
+    public VirtualPetApplication(String c) {
+        catName = c;
+        hunger = 1;
+        thirst = 1;
+        attention = 1;
+        sleepy = 1;
+        bathroom = 1;
     }
 
+    public void changeHunger(int h){
+        hunger += h;
+    }
+    public void changeThirst(int t){
+        thirst += t;
+    }
+    public void changeAttention(int a){
+        attention += a;
+    }
+    public void changeBathroom(int b){
+        bathroom = b;
+    }
+    public void changeSleepy(int s){
+        sleepy += s;
+    }
+
+    // getter methods
+    public String getCatName(){
+        return catName;
+    }
+    public int getAttention(){
+        return attention;
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public int getThirst() {
+        return thirst;
+    }
+    public int getBathroom(){
+        return bathroom;
+    }
+    public int getSleepy(){
+        return sleepy;
+    }
+    // setter methods
+
+
+    public void setAttention(int attention) {
+        this.attention = attention;
+    }
+    public void setBathroom(int bathroom){
+        this.bathroom = bathroom;
+    }
+
+    public void setThirst(int thirst) {
+        this.thirst = thirst;
+    }
+
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+    public void setSleepy(int sleepy) {
+        this.sleepy = sleepy;
+    }
+    // on-tick method that will generate randomized game loop
+    public void tick(){
+        hunger = 1;
+        thirst = 1;
+        attention = 1;
+        sleepy = 1;
+        bathroom = 1;
+    }
 }
